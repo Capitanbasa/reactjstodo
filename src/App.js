f1428091e81b793921a084e2b6d902d0595d3776
Hercival Aragon <hercivalaragon@gmail.com>
2018-01-30 15:22:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+        projects : []
+    }
+  }
+  componentWillMount(){
+    this.setState({ projects : [
+        {
+            title : 'Monday todo',
+            responsible : 'hercival',
+            description : 'Description for monday todo content goes here',
+            priority : 'low'
+        },
+        {
+          title : 'Tuesday todo',
+          responsible : 'hercival',
+          description : 'Description for Tuesday todo take out garbage that pickup by trash collector',
+          priority : 'high'
+        },
+        {
+          title : 'Wednesday todo',
+          responsible : 'lee',
+          description : 'Description for Wednesday todo are normal days',
+          priority : 'medium'
+        },
+        {
+          title : 'Thursday todo',
+          responsible : 'Hyundai',
+          description : 'Description for Thursday todo are normal days',
+          priority : 'high'
+        }
+    ]});
+  }
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
